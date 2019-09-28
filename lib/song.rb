a7033f93d018797adad2e85ddbac6826d6eeb241
Vanna Winland <vgwinland@gmail.com>
2019-09-28 02:21:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,12 @@
 class Song 
   attr_accessor :name, :artist, :genre
   @@count = 0 
-  @@g
+  @@genres = []
+  @@artists = []
+  
   def initialize(name, artist, genre)
     @@count +=1
+    @@
     @name = name
     @artist = artist
     @genre = genre
